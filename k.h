a4980667ecce71edcc99f2c02359e310c35eaddc
Sergey Vidyuk <sergey@kx.com>
2017-07-19 16:11:24 +0100

    update k.h

--- k.h
+++ k.h
@@ -1,13 +1,29 @@
-/* http://kx.com/q/c/c/k.h - latest/definitive version *
- * http://kx.com/q/c/c/readme.txt - documentation      */
-#ifdef WIN32
-typedef __int64 J;
+#ifndef KX
+#define KX
+typedef char*S,C;typedef unsigned char G;typedef short H;typedef int I;typedef long long J;typedef float E;typedef double F;typedef void V;
+#ifdef __cplusplus
+extern"C"{
+#endif
+#if !defined(KXVER)
+#error "Set KXVER=3 for kdb+3.0 or standalone c-api after 2011-04-20. Otherwise set KXVER=2. e.g. #define KXVER 3 or gcc -DKXVER=3"
+#endif
+#if KXVER>=3
+typedef struct k0{signed char m,a,t;C u;I r;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct{J n;G G0[1];};};}*K;
+typedef struct{G g[16];}U;
+#define kU(x) ((U*)kG(x))
+#define xU ((U*)xG)
+extern K ku(U),ktn(I,J),kpn(S,J);
+extern I setm(I);
+#define DO(n,x)	{J i=0,_i=(n);for(;i<_i;++i){x;}}
 #else
-typedef long long J;
-#endif  // typedef struct k0{I r;H t,u;I n;G G0[1];}*K; before gcc3.0
-typedef int I;typedef unsigned char*A,G,*S,C;typedef short H;typedef float E;typedef double F;typedef void V;
 typedef struct k0{I r;H t,u;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct{I n;G G0[1];};};}*K;
-
+extern K ktn(I,I),kpn(S,I);
+#define DO(n,x)	{I i=0,_i=(n);for(;i<_i;++i){x;}}
+#endif
+#ifdef __cplusplus
+}
+#endif
+//#include<string.h>
 // vector accessors, e.g. kF(x)[i] for float&datetime
 #define kG(x)	((x)->G0)
 #define kC(x)	kG(x)
@@ -19,22 +35,28 @@ typedef struct k0{I r;H t,u;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct
 #define kS(x)	((S*)kG(x))
 #define kK(x)	((K*)kG(x))
 
-//      type bytes qtype    ctype  accessor
-#define KB 1  // 1 boolean  char   kG
-#define KG 4  // 1 byte     char   kG
-#define KH 5  // 2 short    short  kH
-#define KI 6  // 4 int      int    kI
-#define KJ 7  // 8 long     int64  kJ
-#define KE 8  // 4 real     float  kE
-#define KF 9  // 8 float    double kF
-#define KC 10 // 1 char     char   kC
-#define KS 11 // * symbol   char*  kS
-#define KM 13 // 4 month    int    kI
-#define KD 14 // 4 date     int    kI (days from 2000.01.01)
-#define KZ 15 // 8 datetime double kF (days from 2000.01.01)
-#define KU 17 // 4 minute   int    kI
-#define KV 18 // 4 second   int    kI
-#define KT 19 // 4 time     int    kI (millisecond)
+//      type bytes qtype     ctype  accessor
+#define KB 1  // 1 boolean   char   kG
+#define UU 2  // 16 guid     U      kU
+#define KG 4  // 1 byte      char   kG
+#define KH 5  // 2 short     short  kH
+#define KI 6  // 4 int       int    kI
+#define KJ 7  // 8 long      long   kJ
+#define KE 8  // 4 real      float  kE
+#define KF 9  // 8 float     double kF
+#define KC 10 // 1 char      char   kC
+#define KS 11 // * symbol    char*  kS
+
+#define KP 12 // 8 timestamp long   kJ (nanoseconds from 2000.01.01)
+#define KM 13 // 4 month     int    kI (months from 2000.01.01)
+#define KD 14 // 4 date      int    kI (days from 2000.01.01)
+
+#define KN 16 // 8 timespan  long   kJ (nanoseconds)
+#define KU 17 // 4 minute    int    kI
+#define KV 18 // 4 second    int    kI
+#define KT 19 // 4 time      int    kI (millisecond)
+
+#define KZ 15 // 8 datetime  double kF (DO NOT USE)
 
 // table,dict
 #define XT 98 //   x->k is XD
@@ -42,10 +64,13 @@ typedef struct k0{I r;H t,u;union{G g;H h;I i;J j;E e;F f;S s;struct k0*k;struct
 
 #ifdef __cplusplus
 extern"C"{
+extern V m9();
+#else
+extern V m9(V);
 #endif
-extern I khpu(char*,I,char*),khp(char*,I),ymd(I,I,I),dj(I);extern V r0(K),sd0(I);extern S sn(S,I),ss(S);
-extern K ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F),kt(I),sd1(I,K(*)(I)),
-  ktn(I,I),knk(I,...),kp(S),kpn(S,I),ja(K*,V*),js(K*,S),jk(K*,K),k(I,char*,...),xT(K),xD(K,K),ktd(K),r1(K),krr(S),orr(S),dot(K,K);
+extern I khpun(const S,I,const S,I),khpu(const S,I,const S),khp(const S,I),okx(K),ymd(I,I,I),dj(I);extern V r0(K),sd0(I),kclose(I);extern S sn(S,I),ss(S);
+extern K ktj(I,J),ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F),kt(I),sd1(I,K(*)(I)),dl(V*f,I),
+ knk(I,...),kp(S),ja(K*,V*),js(K*,S),jk(K*,K),jv(K*k,K),k(I,const S,...)__attribute__((sentinel)),xT(K),xD(K,K),ktd(K),r1(K),krr(const S),orr(const S),dot(K,K),b9(I,K),d9(K);
 #ifdef __cplusplus 
 }
 #endif
@@ -55,17 +80,15 @@ extern K ka(I),kb(I),kg(I),kh(I),ki(I),kj(J),ke(F),kf(F),kc(I),ks(S),kd(I),kz(F)
 #define wh ((I)0x7FFF)
 #define ni ((I)0x80000000)
 #define wi ((I)0x7FFFFFFF)
-#ifdef WIN32
-#define nj ((J)0x8000000000000000)
-#define wj ((J)0x7FFFFFFFFFFFFFFF)
+#define nj ((J)0x8000000000000000LL) 
+#define wj 0x7FFFFFFFFFFFFFFFLL
+#if defined(WIN32) || defined(_WIN32)
 #define nf (log(-1.0))
 #define wf (-log(0.0))
 #define isnan _isnan
 #define finite _finite
-extern double log();
+extern double log(double);
 #else  
-#define nj 0x8000000000000000LL
-#define wj 0x7FFFFFFFFFFFFFFFLL
 #define nf (0/0.0)
 #define wf (1/0.0)
 #define closesocket(x) close(x)
@@ -80,9 +103,7 @@ extern double log();
 #define SW switch
 #define CS(n,x)	case n:x;break;
 #define CD default
-#define DO(n,x)	{I i=0,_i=(n);for(;i<_i;++i){x;}}
 
-#define ZA Z A
 #define ZV Z V
 #define ZK Z K
 #define ZH Z H
@@ -100,7 +121,6 @@ extern double log();
 #define xt x->t
 #define xu x->u
 #define xn x->n
-#define xk TX(K,x)
 #define xx xK[0]
 #define xy xK[1]
 #define xg TX(G,x)
@@ -110,13 +130,17 @@ extern double log();
 #define xe TX(E,x)
 #define xf TX(F,x)
 #define xs TX(S,x)
-#define xB ((G*)xG)
+#define xk TX(K,x)
 #define xG x->G0
 #define xH ((H*)xG)
 #define xI ((I*)xG)
 #define xJ ((J*)xG)
 #define xE ((E*)xG)
 #define xF ((F*)xG)
-#define xC ((C*)xG)
 #define xS ((S*)xG)
 #define xK ((K*)xG)
+#define xC xG
+#define xB ((G*)xG)
+
+#endif
+
